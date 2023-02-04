60d67bc375eb08c27193b18a25b99607ef1a6270
76jin <ranian129@gmail.com>
2023-02-04 17:22:10 +0900

    스프링부트 환경 설정

---
+++ src/main/java/kr/ranian/bootBoard/BootJpaSecurityBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.ranian.bootBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJpaSecurityBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJpaSecurityBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ranian/bootBoard/BootJpaSecurityBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ranian.bootBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaSecurityBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
