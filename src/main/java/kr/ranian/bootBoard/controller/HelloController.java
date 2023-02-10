ff0d4faec464311f7b7b360b9e55acc84b68c02d
76jin <ranian129@gmail.com>
2023-02-10 16:48:47 +0900

    메인화면 메뉴 추가
    - card, jumbotron
    - 글목록 보기, 로그인

--- src/main/java/kr/ranian/bootBoard/controller/HelloController.java
+++ src/main/java/kr/ranian/bootBoard/controller/HelloController.java
@@ -1,14 +1,21 @@
 package kr.ranian.bootBoard.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 	
 	@GetMapping("/hello")
-	public String hello() {
+	public @ResponseBody String hello() {
 		return "Hello Spring Boot";
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "index";
 	}
 
 }
